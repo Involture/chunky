7c5fb2a1e3a3882718a3dbcd8abdb263f64c31d8
Teo <teo.orthlieb@gmail.com>
2023-07-30 16:38:26 +0200

    benched another crate

--- benches/unthbuf.rs
+++ benches/unthbuf.rs
@@ -1,31 +1,35 @@
+use std::num::NonZeroU8;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use chunky::CHUNK_S3;
 use rand::{Rng, seq::SliceRandom};
+use unthbuf::{UnthBuf, aligned::AlignedLayout};
 
-fn rand_read(packed: &Vec<usize>, indices: &Vec<usize>, out: &mut Vec<usize>) {
+fn rand_read(packed: &UnthBuf<AlignedLayout>, indices: &Vec<usize>, out: &mut Vec<usize>) {
     for i in indices.into_iter() {
-        out[*i] = packed[*i];
+        out[*i] = packed.get(*i).unwrap();
     }
 }
 
-fn rand_write(packed: &mut Vec<usize>, indices: &Vec<usize>, input: &Vec<usize>) {
+fn rand_write(packed: &mut UnthBuf<AlignedLayout>, indices: &Vec<usize>, input: &Vec<usize>) {
     for i in indices.into_iter() {
-        packed[*i] = input[*i];
+        packed.set(*i, input[*i]).unwrap();
     }
 }
 
 fn read_benchmark<const BITSIZE: u32>(c: &mut Criterion) {
     let mut rng = rand::thread_rng();
     // generate a random packed array
-    let mut packed = vec![0; CHUNK_S3];
-    packed.fill_with(|| rng.gen_range(0..2_usize.pow(BITSIZE)));
-    let packed = packed.clone();
+    let mut values = vec![0; CHUNK_S3];
+    values.fill_with(|| rng.gen_range(0..2_usize.pow(BITSIZE)));
+    let packed = UnthBuf::new_from_capacity_and_iter(
+        NonZeroU8::try_from(BITSIZE as u8).unwrap(), CHUNK_S3, values.into_iter()
+    );
     // generate random indices
     let mut indices: Vec<usize> = (0..CHUNK_S3).collect();
     indices.shuffle(&mut rng);
     // array for reading into
     let mut values = vec![0; CHUNK_S3];
-    c.bench_function(&format!("cv_rand_read-32^3-{}", BITSIZE), |b| b.iter(|| rand_read(
+    c.bench_function(&format!("ub_rand_read-32^3-{}", BITSIZE), |b| b.iter(|| rand_read(
         black_box(&packed), black_box(&indices), black_box(&mut values)
     )));
 }
@@ -39,11 +43,13 @@ fn write_benchmark<const BITSIZE: u32>(c: &mut Criterion) {
     let mut indices: Vec<usize> = (0..CHUNK_S3).collect();
     indices.shuffle(&mut rng);
     // packed array to write to
-    let mut packed = vec![0; CHUNK_S3];
-    c.bench_function(&format!("cv_rand_write-32^3-{}", BITSIZE), |b| b.iter(|| rand_write(
+    let mut packed = UnthBuf::new_with_default(
+        NonZeroU8::try_from(BITSIZE as u8).unwrap(), CHUNK_S3, 0
+    );
+    c.bench_function(&format!("ub_rand_write-32^3-{}", BITSIZE), |b| b.iter(|| rand_write(
         black_box(&mut packed), black_box(&indices), black_box(&values)
     )));
 }
 
-criterion_group!(control_vec, read_benchmark<4>, write_benchmark<4>);
-criterion_main!(control_vec);
+criterion_group!(unthbuf, read_benchmark<4>, write_benchmark<4>);
+criterion_main!(unthbuf);
